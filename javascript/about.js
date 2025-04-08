061f09e4b5d46c1d234799f160b948548c898046
ZeeanthulIlma <seenathulilma121243@gmail.com>
2025-04-08 08:53:49 +0530

    adding js for stat counts

---
+++ javascript/about.js
@@ -0,0 +1,14 @@
+// Basic counter animation
+const counters = document.querySelectorAll('.stat-box span');
+counters.forEach(counter => {
+    let value = +counter.innerText.replace('+', '');
+    let count = 0;
+    const update = () => {
+        count += 1;
+        counter.innerText = count + "+";
+        if (count < value) {
+            requestAnimationFrame(update);
+        }
+    };
+    update();
+});
